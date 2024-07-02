e5cf19f30c85849675800c8d9eb9aca8e3694119
Admin <admin@Admins-MacBook-Pro-5.local>
2024-07-02 13:47:53 +0530

    models created

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 
 // Passing parameters separately
 const sequelize = new Sequelize('database24', 'keerthana', 'Keerthana1', {
@@ -13,3 +13,52 @@ sequelize.authenticate()
   .catch(err => {
     console.error('Unable to connect to the database:', err);
   });
+
+const Spacestation = sequelize.define(
+  'Spacestation',
+  {
+    Spacestation_id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false,
+    },
+    Spacestation_name: {
+      type: DataTypes.STRING,
+      allownull: false,
+    },
+   Year:{
+    type: DataTypes.INTEGER,
+    allownull: false,
+   },
+   No_of_satelites: {
+    type: DataTypes.INTEGER,
+    allownull: false,
+   },
+  },
+);
+console.log(Spacestation === sequelize.models.Spacestation); 
+
+const Satellites = sequelize.define(
+    'Satellites',
+    {
+      Satellite_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      Satellite_name: {
+        type: DataTypes.STRING,
+        allownull: false,
+      },
+     Launched_year:{
+      type: DataTypes.INTEGER,
+      allownull: false,
+     },
+     Spacestation_id: {
+      type: DataTypes.INTEGER,
+      allownull: false,
+      references: 'spacestation',
+      referencesKey: 'spacestation_id',
+    },
+   },
+  );
+  console.log(Satellites === sequelize.models.Satellites);
